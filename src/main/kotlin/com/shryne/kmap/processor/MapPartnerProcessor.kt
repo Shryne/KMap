67210441dd30c454e5104a1129a43defb6b8f8bc
Shryne <enomeral@gmx.de>
2023-09-19 12:28:20 +0200

    simplify MapPartnerProcessor

--- src/main/kotlin/com/shryne/kmap/processor/MapPartnerProcessor.kt
+++ src/main/kotlin/com/shryne/kmap/processor/MapPartnerProcessor.kt
@@ -5,6 +5,7 @@ import com.shryne.kmap.processor.kmap.check.Checks
 import com.shryne.kmap.processor.kmap.check.KMapSettingCheck
 import com.shryne.kmap.processor.kmap.check.PropertyExistCheck
 import javax.annotation.processing.AbstractProcessor
+import javax.annotation.processing.ProcessingEnvironment
 import javax.annotation.processing.RoundEnvironment
 import javax.lang.model.SourceVersion
 import javax.lang.model.element.Element
@@ -30,81 +31,66 @@ internal class MapPartnerProcessor : AbstractProcessor() {
                     "Source type is: ${source.simpleName}."
                 )
 
-                val mapPartnerMirror = processingEnv
-                    .elementUtils
-                    .getTypeElement(MapPartnerAnnotation::class.java.name)
+                val mp = MP(source, processingEnv)
 
-                for (annotation in source.annotationMirrors) {
-                    if (annotation.annotationType.asElement() == mapPartnerMirror) {
-                        for ((key, value) in annotation.elementValues) {
-                            if (key.simpleName.toString() == "value") {
-                                val target = (value.value as DeclaredType)
-                                    .asElement() as TypeElement
-                                val packageName = annotation.elementValues
-                                    .filter {
-                                        it.key.simpleName.toString() == "packageName"
-                                    }
-                                    .map { it.value.value.toString() }
-                                    .firstOrNull()
+                val target = mp.target
+                val packageName = mp.packageName
 
-                                if (packageName == null) {
-                                    processingEnv.messager.printMessage(
-                                        Diagnostic.Kind.ERROR,
-                                        "MapPartner doesn't contain parameter packageName."
-                                    )
-                                } else {
-                                    processingEnv.messager.printMessage(
-                                        Diagnostic.Kind.NOTE,
-                                        "Target type is: ${target.simpleName}. Source type is ${source.simpleName}."
-                                    )
+                if (packageName == null) {
+                    processingEnv.messager.printMessage(
+                        Diagnostic.Kind.ERROR,
+                        "MapPartner doesn't contain parameter packageName."
+                    )
+                } else {
+                    processingEnv.messager.printMessage(
+                        Diagnostic.Kind.NOTE,
+                        "Target type is: ${target.simpleName}. Source type is ${source.simpleName}."
+                    )
 
-                                    val annotated =
-                                        source.enclosedElements.filter {
-                                            it.getAnnotation(KMapAnnotation::class.java) != null
-                                        }
-
-                                    val kMaps = annotated.map {
-                                        KMap(
-                                            it,
-                                            source,
-                                            target,
-                                            processingEnv.typeUtils
-                                        )
-                                    }
-
-                                    val checks = Checks(
-                                        kMaps.flatMap {
-                                            listOf(
-                                                KMapSettingCheck(
-                                                    it.sourceProperty
-                                                ),
-                                                PropertyExistCheck(
-                                                    it,
-                                                    Clazz(target)
-                                                )
-                                            )
-                                        }
-                                    )
-                                    if (checks.hasErrors()) {
-                                        checks.printErrors(
-                                            processingEnv.messager
-                                        )
-                                    } else {
-                                        MapPartner(
-                                            processingEnv.typeUtils,
-                                            Clazz(source as TypeElement),
-                                            Clazz(target),
-                                            kMaps,
-                                            packageName
-                                        ).writeTo(processingEnv.filer)
-                                    }
-                                }
-                            }
+                    val annotated =
+                        source.enclosedElements.filter {
+                            it.getAnnotation(
+                                KMapAnnotation::class.java
+                            ) != null
                         }
+
+                    val kMaps = annotated.map {
+                        KMap(
+                            it,
+                            source,
+                            target,
+                            processingEnv.typeUtils
+                        )
+                    }
+
+                    val checks = Checks(
+                        kMaps.flatMap {
+                            listOf(
+                                KMapSettingCheck(
+                                    it.sourceProperty
+                                ),
+                                PropertyExistCheck(
+                                    it,
+                                    Clazz(target)
+                                )
+                            )
+                        }
+                    )
+                    if (checks.hasErrors()) {
+                        checks.printErrors(
+                            processingEnv.messager
+                        )
+                    } else {
+                        MapPartner(
+                            processingEnv.typeUtils,
+                            Clazz(source as TypeElement),
+                            Clazz(target),
+                            kMaps,
+                            packageName
+                        ).writeTo(processingEnv.filer)
                     }
                 }
             }
-
         return false
     }
 
@@ -114,5 +100,42 @@ internal class MapPartnerProcessor : AbstractProcessor() {
 
     override fun getSupportedSourceVersion(): SourceVersion {
         return SourceVersion.RELEASE_17
+    }
+
+    class MP(annotated: Element, processingEnv: ProcessingEnvironment) {
+        val target = annotated.annotationMirrors
+            .filter {
+                it.annotationType.asElement().simpleName.toString() == "MapPartner"
+            }
+            .map {
+                it.elementValues
+                    .filter {
+                        it.key.simpleName.toString() == "value"
+                    }
+                    .map {
+                        it.value.value as DeclaredType
+                    }
+                    .map {
+                        it.asElement()
+                    }
+                    .first()
+            }
+            .first() as TypeElement
+
+        val packageName: String? = annotated.annotationMirrors
+            .filter {
+                it.annotationType.asElement().simpleName.toString() == "MapPartner"
+            }
+            .map {
+                it.elementValues
+                    .filter {
+                        it.key.simpleName.toString() == "packageName"
+                    }
+                    .map {
+                        it.value.value.toString()
+                    }
+                    .firstOrNull()
+            }
+            .firstOrNull()
     }
 }
